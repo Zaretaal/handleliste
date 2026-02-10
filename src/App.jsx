3040486e9fd55644361defb7f9022e6e63d1b478
Zaretaal <143384586+Zaretaal@users.noreply.github.com>
2026-02-10 15:01:06 +0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// Dette prosjektet er utviklet med hjelp og veiledning fra ChatGPT, linken er https://chatgpt.com/c/69889bc9-2a70-832f-8b53-cee6facf386c//
+// Dette prosjektet er utviklet med hjelp og veiledning fra ChatGPT. Pga at jeg måtte kjøpe ny pc, så trengte jeg i tillegg til veiledningen be om hjelp om hvordan man reinstallerer alle programmene. linken er https://chatgpt.com/c/69889bc9-2a70-832f-8b53-cee6facf386c//
 import { useState } from "react";
 import AddForm from "./components/AddForm";
 import ShoppingList from "./components/ShoppingList";
